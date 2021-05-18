7fe3f88124ccd50cd3bb3a3c443fdf033b89bb89
Kenson <mfkenson@gmail.com>
2021-05-18 14:55:04 +0800

    --

--- park_demo/park_demo/scripts/launcher/launch_demo_real.py
+++ park_demo/park_demo/scripts/launcher/launch_demo_real.py
@@ -11,8 +11,8 @@ rospy.init_node('launch_demo_world', anonymous=True)
 
 uuid = roslaunch.rlutil.get_or_generate_uuid(None, False)
 roslaunch.configure_logging(uuid)
-launch = roslaunch.parent.ROSLaunchParent(uuid, [home_path + '/5755_ws/src/learning_ros_external_pkgs_noetic/baxter_simulator/baxter_gazebo/launch'])
+launch = roslaunch.parent.ROSLaunchParent(uuid, [home_path + '/5755_ws/src/team-park/park_demo/park_demo/launch/camera.launch'])
 launch.start()
-rospy.loginfo("started Baxter world")
+rospy.loginfo("started Realsense Node")
 rospy.sleep(10)
 rospy.sleep(36000) #10 hours later......
